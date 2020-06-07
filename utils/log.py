abe2766fed182df27cec1076e489e9565057455b
Piotr Węgliński <piotr.weglinski@nordicsemi.no>
2020-06-07 13:37:47 +0200

    basic mesh network can be created

--- utils/log.py
+++ utils/log.py
@@ -5,6 +5,8 @@ logging.basicConfig()
 
 class Loggable:
 
-    def __init__(self):
-        logging.basicConfig()
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        logging.basicConfig(format='%(name)-12s: %(levelname)-8s %(message)s')
         self.logger = logging.getLogger()
+        self.logger.setLevel("DEBUG")
